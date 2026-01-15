4b400fac4867607e9bba6ffa51e5c24010224aee
Marco <marcochan94@hotmail.com>
2026-01-15 22:07:15 +0800

    feat: add functionality to identify users not followed back and those not following the user

--- instachecker.js
+++ instachecker.js
@@ -13,7 +13,9 @@ let user_username = 'lun.lunan'
 
 const fetchUsers = (type, maxId) =>
   fetch(
-    `https://www.instagram.com/api/v1/friendships/${user_id}/${type}/?count=12${maxId ? `&max_id=${maxId}` : ''}&search_surface=follow_list_page`,
+    `https://www.instagram.com/api/v1/friendships/${user_id}/${type}/?count=12${
+      maxId ? `&max_id=${maxId}` : ''
+    }&search_surface=follow_list_page`,
     {
       headers: requestHeaders,
       referrer: `https://www.instagram.com/${user_username}/${type}/`,
@@ -36,5 +38,27 @@ async function fetchAll(type, usernames) {
   }
 }
 
-fetchAll('followers', follower_usernames)
-fetchAll('following', following_usernames)
+await fetchAll('followers', follower_usernames)
+await fetchAll('following', following_usernames)
+
+const arrayDifference = (arr1, arr2) => {
+  // Create sets from the arrays to make comparisons easier
+  const set1 = new Set(arr1)
+  const set2 = new Set(arr2)
+
+  // Use the filter method to find elements in arr1 that are not in arr2
+  const difference = [...set1].filter((item) => !set2.has(item))
+
+  return difference
+}
+
+const accountIDontFollow = arrayDifference(
+  follower_usernames,
+  following_usernames
+)
+console.log("I'm not following: ", accountIDontFollow)
+const accountNotFollowingMe = arrayDifference(
+  following_usernames,
+  follower_usernames
+)
+console.log('Not following me: ', accountNotFollowingMe)
