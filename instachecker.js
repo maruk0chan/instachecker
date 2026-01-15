197f6c7d7c82c286de16867fa5dc969b311c5e74
Marco <marcochan94@hotmail.com>
2026-01-15 20:52:49 +0800

    feat: implement following user fetching functionality

--- instachecker.js
+++ instachecker.js
@@ -7,14 +7,22 @@ let requestHeaders = {
   'x-web-session-id': 'za65qk:juzqbv:1s4e7f',
 }
 let follower_usernames = new Set()
+let following_usernames = new Set()
 let user_id = '9299519515'
 let user_username = 'lun.lunan'
 let followerEndpoint = `https://www.instagram.com/api/v1/friendships/${user_id}/followers/?count=12&search_surface=follow_list_page`
+let followingEndpoint = `https://www.instagram.com/api/v1/friendships/${user_id}/following/?count=12&search_surface=follow_list_page`
 
 const fetchFollowers = (maxId) =>
   fetch(`${followerEndpoint}${maxId ? `&max_id=${maxId}` : ''}`, {
     headers: requestHeaders,
     referrer: `https://www.instagram.com/${user_username}/followers/`,
+  })
+
+const fetchFollowing = (maxId) =>
+  fetch(`${followingEndpoint}${maxId ? `&max_id=${maxId}` : ''}`, {
+    headers: requestHeaders,
+    referrer: `https://www.instagram.com/${user_username}/following/`,
   })
 
 async function fetchAllFollowers() {
@@ -32,5 +40,20 @@ async function fetchAllFollowers() {
     }
   }
 }
-
+async function fetchAllFollowing() {
+  let nextMaxId
+  while (true) {
+    try {
+      const json = await (await fetchFollowing(nextMaxId)).json()
+      json.users.forEach((user) => following_usernames.add(user.username))
+      console.log(json)
+      if (!json.next_max_id) break
+      nextMaxId = json.next_max_id
+    } catch (error) {
+      console.error(error)
+      break
+    }
+  }
+}
 fetchAllFollowers()
+fetchAllFollowing()
